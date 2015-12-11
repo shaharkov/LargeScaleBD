33c59bd2de94e10a92860dffc72acdd92fd2bcee
shaharkov <shaharkov@gmail.com>
2015-12-11 23:16:32 +0200

    add header

--- colStack.m
+++ colStack.m
@@ -1,3 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Code implementing the paper "Large-Scale Bounded Distortion Mappings".
+% Disclaimer: The code is provided as-is for academic use only and without any guarantees. 
+%             Please contact the author to report any bugs.
+% Written by Shahar Kovalsky (http://www.wisdom.weizmann.ac.il/~shaharko/)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function y = colStack(x)
 
 y = x(:);
